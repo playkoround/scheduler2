ee42cbb332bd51df11633032194f664c1ab52743
playkoround <seasouphan@gmail.com>
2025-05-16 00:02:21 +0900

    Feat: make CommentService

--- src/main/java/com/sparta/scheduler/entity/Comment.java
+++ src/main/java/com/sparta/scheduler/entity/Comment.java
@@ -13,7 +13,7 @@ public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String content;
 
--- src/main/java/com/sparta/scheduler/entity/Reply.java
+++ src/main/java/com/sparta/scheduler/entity/Reply.java
@@ -11,7 +11,7 @@ public class Reply {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String content;
 
